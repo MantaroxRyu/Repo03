d29dbf4df53760bd30f64f0509269c7bd0b94c2c
MantaroxRyu <mr.dinot@gmail.com>
2021-09-16 10:59:22 +0200

    ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var numOne = 15;
+    var numTwo = 8;
+    var sum = Number(numOne) + Number(numTwo);
+    
+    // Only change code above this line
+    
+    return sum;
+
+    }
+    
+    console.log(main("The sum is: " + sum)); // change this line
+
+    module.exports = main;
