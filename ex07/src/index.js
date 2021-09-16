7069f7d9547d4164d927bd87b4633ec7b1aa14b5
MantaroxRyu <mr.dinot@gmail.com>
2021-09-16 10:59:12 +0200

    ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,28 @@
+function main() {
+    // Only change code below this line
+    // Variable declarations
+
+var studlyCapVar
+var properCamleCase
+var titleCaseOver
+
+    // Variable assignments
+
+    studlyCapVar = 10;
+    properCamleCase = "A String";
+    titleCaseOver = 9000;
+
+    // Only change code above this line
+    
+    return {
+
+    studlyCapVar,
+    properCamleCase,
+    titleCaseOver,
+    }
+
+}
+    
+    console.log(main());
+
+    module.exports = main;
